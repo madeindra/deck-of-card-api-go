c44da46754a55f84021fbee233724e5b90217fc9
Made Indra <made.indra@protonmail.com>
2022-07-27 11:38:41 +0700

    feat: add handler logic

--- api/v1/deck/handler.go
+++ api/v1/deck/handler.go
@@ -1,7 +1,10 @@
 package deck
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
@@ -24,13 +27,69 @@ func NewDeckHandler(router *mux.Router, validate *validator.Validate, usecase De
 }
 
 func (handler *DeckHandler) Create(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 
+	shuffleParam := r.URL.Query().Get("shuffled")
+	cardsParam := r.URL.Query().Get("cards")
+
+	isShuffled := false
+	cardsList := []string{}
+
+	if shuffleParam != "" {
+		isShuffled, _ = strconv.ParseBool(shuffleParam)
+	}
+
+	if cardsParam != "" {
+		cards := strings.Split(cardsParam, ",")
+		cardsList = append(cardsList, cards...)
+	}
+
+	result := handler.Usecase.Create(ctx, isShuffled, cardsList)
+
+	response, err := json.Marshal(result)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write([]byte("{message: \"An Error Occured\"}"))
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(response)
 }
 
 func (handler *DeckHandler) FindByID(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 
+	uuid := mux.Vars(r)["uuid"]
+	result := handler.Usecase.FindByID(ctx, uuid)
+
+	response, err := json.Marshal(result)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write([]byte("{message: \"An Error Occured\"}"))
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(response)
 }
 
 func (handler *DeckHandler) Draw(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 
+	uuid := mux.Vars(r)["uuid"]
+	result := handler.Usecase.Draw(ctx, uuid)
+
+	response, err := json.Marshal(result)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write([]byte("{message: \"An Error Occured\"}"))
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(response)
 }
